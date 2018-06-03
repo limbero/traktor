397ee84896f6ef834e6595f3897e28d29efbf7d7
Axel Samuelsson <harby@spotify.com>
2018-06-03 12:05:45 +0200

    Fix build

--- src/AuthRedirect.jsx
+++ src/AuthRedirect.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
-import qs from 'query-string';
+import qs from 'qs';
 import Helpers from './Helpers';
 
 import store from "./store/index";
@@ -13,7 +13,7 @@ class AuthRedirect extends Component {
 
   async componentDidMount() {
     console.log(Helpers);
-    const getParams = qs.parse(this.props.location.search);
+    const getParams = qs.parse(this.props.location.search.slice(1));
     let token = await Helpers.fetchJson('https://api.trakt.tv/oauth/token', 'POST', {
       code: getParams.code,
       client_id: env.REACT_APP_TRAKT_CLIENT_ID,
