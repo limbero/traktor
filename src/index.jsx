6725d5048ffce387fcf6b1051589a501c09ffd6c
Axel Samuelsson <harby@spotify.com>
2019-08-15 22:13:15 -0400

    remove service worker, it causes weird caching issues and I am not making use of it

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
