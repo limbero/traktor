d247ba835a098b27885bd1f891882dd4958acca7
Axel Samuelsson <harby@spotify.com>
2020-02-09 21:01:17 -0500

    Add service worker so iOS can update

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Router";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById("root"));
+
+serviceWorker.register();
