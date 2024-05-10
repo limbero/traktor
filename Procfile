ee579717d0b4d1fdccef1ff9264f24c4ef7d232d
Axel Samuelsson <samuelsson.axel@gmail.com>
2024-05-10 18:13:51 +0200

    bun+vite on heroku?

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bun run build && bun run preview
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router.jsx';
 import * as serviceWorker from './serviceWorker.js';
+import { SyntheticEvent } from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Router />,
 );
 
 serviceWorker.register({
   // kudos to https://stackoverflow.com/a/58596965
-  onUpdate: (registration: any) => {
+  onUpdate: (registration) => {
     const waitingServiceWorker = registration.waiting;
 
     if (waitingServiceWorker) {
